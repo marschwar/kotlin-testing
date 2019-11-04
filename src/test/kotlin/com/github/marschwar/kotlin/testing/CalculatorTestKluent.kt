bfcc81852399de7a833f32e2b8939347e086948b
Markus Schwarz <post@markus-schwarz.net>
2019-11-04 21:53:43 +0100

    add chained assertions for kluent

--- src/test/kotlin/com/github/marschwar/kotlin/testing/CalculatorTestKluent.kt
+++ src/test/kotlin/com/github/marschwar/kotlin/testing/CalculatorTestKluent.kt
@@ -1,6 +1,11 @@
 package com.github.marschwar.kotlin.testing
 
+
+import org.amshove.kluent.`should be greater than`
+import org.amshove.kluent.`should be in range`
 import org.amshove.kluent.`should equal`
+import org.amshove.kluent.shouldBeInRange
+import org.amshove.kluent.shouldBePositive
 import org.amshove.kluent.shouldEqual
 import kotlin.test.Test
 
@@ -12,13 +17,14 @@ class CalculatorTestKluent {
     fun `correctly adds two numbers`() {
         val result = subject.add(1, 2)
 
-        result.shouldEqual(3)
+        result.shouldBeInRange(2, 4)
+            .shouldEqual(3)
     }
 
     @Test
     fun `correctly adds multiple numbers`() {
         val result = subject.add(1, 2, 3, 4)
 
-        result `should equal` 10
+        result `should be in range` 5..15 and result `should equal` 10
     }
 }
